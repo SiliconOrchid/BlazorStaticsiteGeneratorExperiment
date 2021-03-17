1ec186ac3264dc4dfc397a28a3187ea4df792b45
Jim McG <jimmcgowan.uk@gmail.com>
2021-03-17 12:46:49 +0000

    create VS solution and empty templated projects

---
+++ BlazorStaticSiteGeneratorExperiment.BuildAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorStaticSiteGeneratorExperiment.BuildAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
